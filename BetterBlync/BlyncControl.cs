e46f0dfc2d1f927c0333be363e593aec0e18d369
Reese <rfreeman161@gmail.com>
2016-01-21 11:34:39 -0800

    Initial Build
    * Added basic light controls
    * Toying around with capabilities in the GUI

---
+++ BetterBlync/BlyncControl.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blynclight;
+
+namespace BetterBlync
+{
+    public class BlyncControl
+    {
+        private BlynclightController blync;
+        private int lightCount, deviceSelection;
+
+        public enum BlyncColor
+        {
+            Green,
+            Blue,
+            Purple,
+            Yellow,
+            Red
+        }
+
+        public BlyncControl()
+        {
+            blync = new BlynclightController();
+            lightCount = findBlyncLights();
+            if(lightCount == 0 )
+            {
+                // TODO create custom exception
+            }
+            // TODO see what happens with multiple connected lights
+            // if ( lightCount == 1 ) deviceSelection = 0;
+            deviceSelection = 0;
+
+            // Default color is green
+            ChangeToGreen();
+            CurrentColor = BlyncColor.Green;
+        }
+
+        public BlyncColor CurrentColor { get; private set; }
+
+        private int findBlyncLights()
+        {
+            return blync.InitBlyncDevices();
+        }
+
+        public void CloseDevices()
+        {
+            blync.CloseDevices( lightCount );
+        }
+
+        public void ChangeToGreen()
+        {
+            blync.TurnOnGreenLight( deviceSelection );
+            CurrentColor = BlyncColor.Green;
+        }
+
+        public void ChangeToBlue()
+        {
+            blync.TurnOnBlueLight( deviceSelection );
+            CurrentColor = BlyncColor.Blue;
+        }
+
+        public void ChangeToYellow()
+        {
+            blync.TurnOnYellowLight( deviceSelection );
+            CurrentColor = BlyncColor.Yellow;
+        }
+
+        public void ChangeToPurple()
+        {
+            blync.TurnOnMagentaLight( deviceSelection );
+            CurrentColor = BlyncColor.Purple;
+        }
+
+        public void ChangeToRed()
+        {
+            blync.TurnOnRedLight( deviceSelection );
+            CurrentColor = BlyncColor.Red;
+        }
+
+        public void ResetLight()
+        {
+            blync.ResetLight( deviceSelection );
+            ChangeToGreen();
+            CurrentColor = BlyncColor.Green;
+        }
+
+        public void TurnOffLight()
+        {
+            blync.ResetLight( deviceSelection );
+        }
+    }
+}
---
+++ BetterBlync/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BetterBlync
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BlyncControl blync;
+        public MainWindow()
+        {
+            InitializeComponent();
+            blync = new BlyncControl();
+        }
+
+        private void btnBlue_Click(object sender, RoutedEventArgs e)
+        {
+            blync.ChangeToBlue();
+        }
+
+        private void btnGreen_Click(object sender, RoutedEventArgs e)
+        {
+            blync.ChangeToGreen();
+        }
+
+        private void btnRed_Click(object sender, RoutedEventArgs e)
+        {
+            blync.ChangeToRed();
+        }
+
+        private void btnYellow_Click(object sender, RoutedEventArgs e)
+        {
+            blync.ChangeToYellow();
+        }
+
+        private void btnPurple_Click(object sender, RoutedEventArgs e)
+        {
+            blync.ChangeToPurple();
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            blync.ResetLight();
+        }
+
+        private void btnShutoff_Click(object sender, RoutedEventArgs e)
+        {
+            blync.TurnOffLight();
+        }
+
+        private void btnParty_Click(object sender, RoutedEventArgs e)
+        {
+            System.Threading.Thread th = new System.Threading.Thread( PartyOn );
+            th.Start();
+        }
+
+        private void PartyOn()
+        {
+            int interval = 100;
+            int count = 10;
+            do
+            {
+                System.Threading.Thread.Sleep( interval );
+                blync.ChangeToBlue();
+                System.Threading.Thread.Sleep( interval );
+                blync.ChangeToRed();
+                System.Threading.Thread.Sleep( interval );
+                blync.ChangeToPurple();
+                System.Threading.Thread.Sleep( interval );
+                blync.ChangeToYellow();
+                System.Threading.Thread.Sleep( interval );
+                blync.ChangeToGreen();
+                count--;
+            } while ( count > 0 );
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            blync.CloseDevices();
+        }
+    }
+}
